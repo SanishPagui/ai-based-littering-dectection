eaf53add3cbae79968778542875495c6c1875e17
Jai Dessai <jaistudymail13@gmail.com>
2025-04-10 16:39:15 +0530

    Dock

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from '@stackframe/stack'
 import React from 'react'
+import { FloatingDockDemo } from '../components/dock'
 
 const page = () => {
   return (
     <div>
       <UserButton/>
+      <FloatingDockDemo />
     </div>
   )
 }
